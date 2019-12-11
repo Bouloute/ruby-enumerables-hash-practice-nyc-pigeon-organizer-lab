86839e66b8cef58b44eaf98fdf95a45db234dc63
Anna Lewis Dagnino <annaick1995@gmail.com>
2019-12-11 05:15:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,17 +5,11 @@ def nyc_pigeon_organizer(data)
   data.each{|discription, detail_discription| 
     detail_discription.each{|pigeon_discription, pigeon_name|
       pigeon_name.each{|name|
-      puts name
-      puts discription
-      puts pigeon_discription
-      puts
-      #new_hash[name][discription] = "test"
-        #new_hash[name][discription] = pigeon_discription
-        #new_hash[name] = {[discription] = pigeon_discription
-        #testing = {discription => pigeon_discription}
-        new_hash[discription] =
-        #new_hash[discription] = "#{pigeon_discription} #{name}"
-        pp new_hash
+      
+      new_hash.each{|sorted_discription, sorted_detail|
+        new_hash[discription].each{||}
+      }
+      
       }
     }
   }
